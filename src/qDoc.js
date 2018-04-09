fad5f238ce20196d19170a04178b2c73bc1e12eb
Yianni Ververis <yiannisver@gmail.com>
2018-04-09 14:13:29 -0400

    add identity config param to both qDoc and qApp

--- src/qDoc.js
+++ src/qDoc.js
@@ -1,13 +1,9 @@
-import utility from './utilities/';
-
 const enigma = require('enigma.js');
 const schema = require('enigma.js/schemas/12.20.0.json');
 const SenseUtilities = require('enigma.js/sense-utilities');
 
 
 const qDoc = async (config) => {
-  const myConfig = config;
-  myConfig.identity = utility.uid(16);
   const url = SenseUtilities.buildUrl(config);
   const session = enigma.create({ schema, url });
   const global = await session.open();
