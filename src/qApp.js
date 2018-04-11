a88d780ebff341a86c1449ade522a6050407ec03
Yianni Ververis <yiannisver@gmail.com>
2018-04-11 09:01:41 -0400

    fix the prefix for qApp

--- src/qApp.js
+++ src/qApp.js
@@ -33,7 +33,7 @@ const qApp = async (config) => {
     });
     return new Promise((resolve) => {
       window.require(['js/qlik'], (qlik) => {
-        const app = qlik.openApp(config.appId, { ...config, isSecure: config.secure });
+        const app = qlik.openApp(config.appId, { ...config, isSecure: config.secure, prefix });
         resolve(app);
       });
     });
