27db017ae67463ecabfd54b6986ab7edebbee9b3
Yianni Ververis <yiannisver@gmail.com>
2018-04-16 12:07:22 -0400

    add domPointImage picasso component

--- src/picasso/settings/index.js
+++ src/picasso/settings/index.js
@@ -2,7 +2,8 @@ import horizontalBarchart from './horizontalBarchart';
 import verticalBarchart from './verticalBarchart';
 import piechart from './piechart';
 import scatterplot from './scatterplot';
+import scatterplotImage from './scatterplotImage';
 
 export default {
-  horizontalBarchart, verticalBarchart, piechart, scatterplot,
+  horizontalBarchart, verticalBarchart, piechart, scatterplot, scatterplotImage,
 };
