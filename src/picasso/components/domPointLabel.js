bc3a5267c119d2198a30264c268dede5ea704946
Yianni Ververis <yiannisver@gmail.com>
2018-04-20 12:15:14 -0400

    collision detection

--- src/picasso/components/domPointLabel.js
+++ src/picasso/components/domPointLabel.js
@@ -8,15 +8,36 @@ export default {
     this.size = options.size;
   },
   generatePoints(data) {
+    let visible = [];
     return data.items.map((row) => {
+      const left = (this.size.width * row.x) - (data.settings.width / 2);
+      const top = (this.size.height * row.y) - (data.settings.height / 2) - data.settings.offset;
+      let display = 'inline';
+      const p = { w: data.settings.width, h: data.settings.height };
+      // Collision detection
+      if (visible.length) {
+        for (const n of visible) {
+          if (
+            left < n.left + p.w &&
+                left + p.w > n.left &&
+                top < n.top + p.h &&
+                p.h + top > n.top
+          ) {
+            display = 'none';
+            break;
+          }
+        }
+      }
+      if (display === 'inline') visible = [...visible, { left, top }];
       const style = {
         position: 'absolute',
-        left: `${(this.size.width * row.x) - (data.settings.width / 2)}px`,
-        top: `${(this.size.height * row.y) - (data.settings.height / 2) - data.settings.offset}px`,
+        left: `${left}px`,
+        top: `${top}px`,
         'font-size': (data.settings.fontSize) ? data.settings.fontSize : '10px',
         color: (data.settings.color) ? data.settings.color : '#000000',
         width: `${data.settings.width}px`,
         'text-align': 'center',
+        display,
       };
       const res = this.h('div', {
         attrs: {
