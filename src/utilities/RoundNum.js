47fe0551944ce23816e497d78a8803f8c922ae53
Yianni Ververis <yiannisver@gmail.com>
2018-06-26 14:53:39 -0400

    new build for the roundNum if <1000 was not working

--- src/utilities/RoundNum.js
+++ src/utilities/RoundNum.js
@@ -2,7 +2,7 @@ function roundNumber(numb, precision) {
   let num = numb;
   // check if the string passed is number or contains formatting like 13%
   if (/^[0-9.]+$/.test(num)) {
-    num = (precision && num > 0) ? parseFloat(num).toFixed(2) : Math.round(num);
+    num = (precision && num > 1000) ? parseFloat(num).toFixed(2) : Math.round(num);
     if (num >= 1000 && num < 1000000) {
       num = (precision) ? parseFloat(num / 1000).toFixed(2) : Math.round(num / 1000);
       if (/\.00$/.test(num)) {
