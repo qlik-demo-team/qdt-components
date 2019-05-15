1596a05d30ef1e1456b921cd19c350cd96bc45ab
yiannisver <yiannisver@gmail.com>
2019-05-15 09:14:04 -0400

    add vertical group barchart

--- src/picasso/settings/verticalGroupBarchart.js
+++ src/picasso/settings/verticalGroupBarchart.js
@@ -1,18 +1,23 @@
-// https://observablehq.com/@miralemd/picasso-js-grouped-bar-chart
-import { axis, box, tooltip, labels, range } from './components';
+import {
+  axis, tooltip, labels, range, box,
+} from './components'; // box
 import { itooltip, pan } from './interactions';
 import theme from '../../styles';
 
 const setting = {
   scales: {
     x: { data: { extract: { field: 'qDimensionInfo/0' } }, padding: 0.3 },
-    y: { data: { field: 'qMeasureInfo/0' }, include: [0], invert: true },
+    y: { data: { fields: ['qMeasureInfo/0', 'qMeasureInfo/1'] }, include: [0], invert: true },
   },
   components: [
     axis(),
     axis({ scale: 'y' }),
-    box({ fill: theme.primary, stroke: theme.primaryLight }),
-    box({ fill: theme.secondary, stroke: theme.secondaryLight }),
+    box({
+      displayOrder: 0, measures: 2, end: { field: 1 }, fill: theme.primary, stroke: theme.primaryLight,
+    }),
+    box({
+      displayOrder: 1, measures: 2, end: { field: 2 }, fill: theme.secondary, stroke: theme.secondaryLight,
+    }),
     range(),
     labels(),
     tooltip,
