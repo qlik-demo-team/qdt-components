1a9d98048cd8fb26db02649b8f6eca32f12388f5
Ryan Arpe <ryan_arpe@hotmail.com>
2019-11-11 21:03:39 +0000

    Update mekkoLabels.js

--- src/components/QdtPicasso/picasso/settings/components/mekkoLabels.js
+++ src/components/QdtPicasso/picasso/settings/components/mekkoLabels.js
@@ -1,1 +1,41 @@
+const labels = function labels({
+  key = 'labels',
+  component = 'cell',
+  selector = 'rect',
+  displayOrder = 2,
+  fontSize = 12,
+  type = 'rows',
+  insideFill = '#FFFFFF',
+  outsideFill = '#666666',
+} = {}) {
+  const comp = {
+    type: 'labels',
+    dock,
+    key,
+    displayOrder,
+    settings: {
+      sources: [{
+        component,
+        selector,
+        strategy: {
+          type,
+          settings: {
+            fontSize,
+            fill: insideFill,
+            labels: [{
+               label: d => d.data ? d.data.label : ''
+            }, {
+               label: d => d.data ? d.formatter(format)(d.data.end.value - d.data.start.value): ''
+            }, {
+               label: d => d.data ? (d.data.metric.value).toFixed(0) : ''
+             }]
+          },
+        },
+      }],
+    },
+  };
 
+  return comp;
+};
+
+export default labels;
