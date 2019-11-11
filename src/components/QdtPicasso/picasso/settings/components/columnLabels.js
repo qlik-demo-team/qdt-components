7255b890af6ce39294d3825c2b18f06dbbe27cf1
Ryan Arpe <ryan_arpe@hotmail.com>
2019-11-11 21:10:17 +0000

    Update columnLabels.js

--- src/components/QdtPicasso/picasso/settings/components/columnLabels.js
+++ src/components/QdtPicasso/picasso/settings/components/columnLabels.js
@@ -1,1 +1,42 @@
+const mekkoLabels = function mekkoLabels({
+  key = 'labels',
+  component = 'bar-labels',
+  dock = '@bar-labels',
+  selector = 'rect',
+  displayOrder = 2,
+  fontSize = 12,
+  type = 'rows',
+  insideFill = '#FFFFFF',
+  outsideFill = '#666666',
+} = {}) {
+  const comp = {
+    type: 'labels',
+    dock,
+    key,
+    displayOrder,
+    settings: {
+      sources: [{
+        component,
+        selector,
+        strategy: {
+          type,
+          settings: {
+            fontSize,
+            fill: insideFill,
+            labels: [{
+               linkData({ node }) { return node.data; }, label: d => { if (!d.data) { return ''; }
+                  return `${d.data.series.label} (${d.formatter(format)(d.data.end.value - d.data.start.value)})`;},
+                },
+                {
+                linkData({ node }) { return node.data; }, label: d => (d.data ? `${d.data.metric.label}` : '') 
+                }],                    
+          },
+        },
+      }],
+    },
+  };
 
+  return comp;
+};
+
+export default mekkoLabels;
