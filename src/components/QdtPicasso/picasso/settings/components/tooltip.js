8674476d0291e6d4d963916d88dd20d1fbfea75a
yiannisver <yiannisver@gmail.com>
2019-12-02 08:23:30 -0500

    fix tooltips for all charts

--- src/components/QdtPicasso/picasso/settings/components/tooltip.js
+++ src/components/QdtPicasso/picasso/settings/components/tooltip.js
@@ -51,14 +51,13 @@ const component = {
     },
     // Generate virtual nodes
     content: ({ h, data }) => {
-      console.log(data);
       let html = '';
       switch (data[0].key) {
         // line, dots
         case 'point':
         case 'point2':
           html = h('div.qdt-tooltip-header', {}, [
-            h('div.qdt-tooltip-header-title', {}, `${data[0].data.x.label}: `),
+            h('div.qdt-tooltip-header-title', {}, `${data[0].data.label}: `),
             h('div.qdt-tooltip-header-measure', {}, `${data[0].data.y.label}`),
           ]);
           break;
@@ -69,46 +68,22 @@ const component = {
             h('div.qdt-tooltip-header-measure', {}, `${data[0].data.num.label}`),
           ]);
           break;
-        // Barcharts
+        // Barcharts, Gauge
         case 'bar':
         default:
-          html = h('div.qdt-tooltip-header', {}, [
-            h('div.qdt-tooltip-header-title', {}, `${data[0].data.label}: `),
-            h('div.qdt-tooltip-header-measure', {}, `${data[0].data.end.label}`),
-          ]);
+          if (data[0].data.series) { // Stacked
+            html = h('div.qdt-tooltip-header', {}, [
+              h('div.qdt-tooltip-header-title', { align: 'center', style: { 'border-bottom': '1px solid rgba(255,255,255,0.2)', 'padding-bottom': '5px', display: 'block' } }, `${data[0].data.label}`),
+              h('div.qdt-tooltip-header-measure', { align: 'center', style: { 'padding-top': '5px' } }, `${data[0].data.series.label}: ${data[0].data.end.value}`),
+            ]);
+          } else {
+            html = h('div.qdt-tooltip-header', {}, [
+              h('div.qdt-tooltip-header-title', {}, `${data[0].data.label}: `),
+              h('div.qdt-tooltip-header-measure', {}, `${data[0].data.end.label}`),
+            ]);
+          }
           break;
       }
-      // else if (data[0].data.length && data[0].data.props.length === 1) { // Pie, scatterplot, line
-      //   html = h('div.qdt-tooltip-header', {}, [
-      //     h('div.qdt-tooltip-header-box', {
-      //       style: { backgroundColor: data[0].color },
-      //     }, ''),
-      //     h('div.qdt-tooltip-header-title', {}, `${data[0].title}: `),
-      //     h('div.qdt-tooltip-header-measure', {}, `${data[0].props[0].value}`),
-      //   ]);
-      // } else if (data[0].data.length && data[0].props.length === 2) { // Single Barchart
-      //   html = h('div.qdt-tooltip-header', {}, [
-      //     h('div.qdt-tooltip-header-box', {
-      //       style: { backgroundColor: data[0].color },
-      //     }, ''),
-      //     h('div.qdt-tooltip-header-title', {}, `${data[0].title}: `),
-      //     h('div.qdt-tooltip-header-measure', {}, `${data[0].props[1].value}`),
-      //   ]);
-      // } else if (data.length && data[0].props.length === 3) {
-      //   html = h('div.qdt-tooltip-header', {}, [
-      //     h('div.qdt-tooltip-header-box', {
-      //       style: { backgroundColor: data[0].color },
-      //     }, ''),
-      //     h('div.qdt-tooltip-header-title', {}, `${data[0].props[0].value}: `),
-      //     h('div.qdt-tooltip-header-measure', {}, `${data[0].props[1].value}`),
-      //   ]);
-      // } else if (data.length && data[0].props.length === 4) { // Mekko
-      //   html = h('div', {}, [
-      //     h('div', { align: 'center', style: { 'border-bottom': '1px solid rgba(255,255,255,0.2)', 'padding-bottom': '5px' } }, `${data[0].props[0].value}`),
-      //     h('div', { align: 'center', style: { 'padding-top': '5px' } }, `${data[0].title} (${Math.round((data[0].props[2].value - data[0].props[3].value) * 100)}%)`),
-      //     h('div', { align: 'center' }, `${data[0].props[1].value}`),
-      //   ]);
-      // }
       return h('div', { display: 'table' }, html);
     },
     afterShow({ element }) {
