1ae76d73b6203cac1efe379176b87bd05cbd9df3
Francis Kabinoff <francis.kabinoff@gmail.com>
2020-02-07 10:27:17 -0500

    fix nova

--- src/qdtNova.jsx
+++ src/qdtNova.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { useApp, useLayout, useModel, useElement, useEffect } from '@nebula.js/supernova';
 
 export default ({ component: Component, options }) => ((env) => ({
   qae: {},
-  component: {
-    created() {
-      console.log('created', env); //eslint-disable-line
-    },
-    render({ layout, model, element }) {
-      ReactDOM.render(<Component layout={layout} model={model} options={options} />, element);
-    },
-  },
+  component() {
+    const app = useApp();
+    const layout = useLayout();
+    const model = useModel();
+    const element = useElement();
+    useEffect(() => {
+      ReactDOM.render(<Component app={app} layout={layout} model={model} options={options} />, element);
+      return(() => {
+        ReactDOM.unmountComponentAtNode(element);
+      })
+    }, [app, layout, model, element]);
+  }
 }));
