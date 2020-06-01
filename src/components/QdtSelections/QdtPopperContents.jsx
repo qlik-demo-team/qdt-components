d311a40995a0b04203f0ee183fd6723dde52a0b8
yiannisver <yiannisver@gmail.com>
2020-06-01 11:20:45 -0400

    fix: QdtSelections - disable search for now and sort by state

--- src/components/QdtSelections/QdtPopperContents.jsx
+++ src/components/QdtSelections/QdtPopperContents.jsx
@@ -2,9 +2,9 @@ import React, { useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import {
-  MenuList, MenuItem, Input, ListItemIcon, ListItemText,
+  MenuList, MenuItem, ListItemIcon, ListItemText, // , Input
 } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
+// import SearchIcon from '@material-ui/icons/Search';
 import CheckIcon from '@material-ui/icons/Check';
 import useSessionObject from '../../hooks/useSessionObject';
 
@@ -15,6 +15,7 @@ const QdtSelectionsPopper = ({ open, qField, app }) => {
       qListObjectDef: {
         qDef: {
           qFieldDefs: [qField],
+          qSortCriterias: [{ qSortByState: 1 }],
         },
         qInitialDataFetch: [{
           qWidth: 1,
@@ -28,9 +29,9 @@ const QdtSelectionsPopper = ({ open, qField, app }) => {
     model.selectListObjectValues('/qListObjectDef', [qElemNumber], true, true);
   }, [model]);
 
-  const handleSearch = useCallback((event) => {
-    model.searchListObjectFor('/qListObjectDef', event.target.value);
-  }, [model]);
+  // const handleSearch = useCallback((event) => {
+  //   model.searchListObjectFor('/qListObjectDef', event.target.value);
+  // }, [model]);
 
   useEffect(() => {
     if (open && model) model.beginSelections(['/qListObjectDef']);
@@ -39,12 +40,12 @@ const QdtSelectionsPopper = ({ open, qField, app }) => {
 
   return (
     <MenuList component="nav">
-      <MenuItem>
+      {/* <MenuItem>
         <ListItemIcon>
           <SearchIcon />
         </ListItemIcon>
         <Input type="search" onChange={handleSearch} disableUnderline />
-      </MenuItem>
+      </MenuItem> */}
       { layout?.qListObject?.qDataPages[0]?.qMatrix.map((row) => (
         <MenuItem
           key={row[0].qElemNumber}
